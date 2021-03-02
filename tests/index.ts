993c2a3aac790b89d81533967cc3af737a1db7a7
Michael Bock <me@thought2.de>
2021-03-02 13:58:55 +0100

    Add `insertNode` test

--- tests/index.ts
+++ tests/index.ts
@@ -13,4 +13,22 @@ describe('index', () => {
       });
     });
   });
+
+  describe('Combinators', () => {
+    describe('insertNode', () => {
+      it('should add a new node', () => {
+        deepStrictEqual(
+          fp.function.pipe(
+            graph.empty<string, string, string>(),
+            graph.insertNode(fp.eq.eqString)('n1', 'Node 1'),
+            graph.entries
+          ),
+          {
+            nodes: [['n1', 'Node 1']],
+            edges: [],
+          }
+        );
+      });
+    });
+  });
 });
